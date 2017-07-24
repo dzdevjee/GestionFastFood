215f28b02d6e387d341a4157753356843dae0a60
yahiaBrahim <goospearsgle@gmail.com>
2017-07-24 16:09:46 +0100

    Create Plat.java
    Ajoute Class "Plat"

---
+++ GFastFood/src/main/java/Plat.java
@@ -0,0 +1,30 @@
+public class Plat {
+	private int idPlat;
+	private String nomPlat;
+	private double prixPlat;
+
+	public int getIdPlat() {
+		return idPlat;
+	}
+
+	public void setIdPlat(int idPlat) {
+		this.idPlat = idPlat;
+	}
+
+	public String getNomPlat() {
+		return nomPlat;
+	}
+
+	public void setNomPlat(String nomPlat) {
+		this.nomPlat = nomPlat;
+	}
+
+	public double getPrixPlat() {
+		return prixPlat;
+	}
+
+	public void setPrixPlat(double prixPlat) {
+		this.prixPlat = prixPlat;
+	}
+
+}
