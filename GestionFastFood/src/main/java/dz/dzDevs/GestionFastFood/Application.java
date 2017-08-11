9e532fd257047fe2f7514e7f4682dfefaf905741
dzdevjee <30406448+dzdevjee@users.noreply.github.com>
2017-08-11 20:11:32 +0200

    Update Application.java

--- GestionFastFood/src/main/java/dz/dzDevs/GestionFastFood/Application.java
+++ GestionFastFood/src/main/java/dz/dzDevs/GestionFastFood/Application.java
@@ -1,7 +1,6 @@
 Package dz.dzDevs.GestionFastFood
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -13,8 +12,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @SpringBootApplication
 
 public class Application {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
 
     public static void main(String[] args) {
         LOGGER.info(">>> Entered application...");
