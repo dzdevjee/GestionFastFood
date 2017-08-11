5bf1a9c2a217f9d3ce5afff75fc9535538df0e50
dzDev <dzdevjee@gmail.com>
2017-08-11 17:58:11 +0200

    [ADD] RoleDAOImpl

--- GestionFastFood/src/main/java/dz/dzDevs/GestionFastFood/DAO/IRoleDAO.java
+++ GestionFastFood/src/main/java/dz/dzDevs/GestionFastFood/DAO/IRoleDAO.java
@@ -7,6 +7,7 @@ public interface IRoleDAO {
 	public void 		createRole(Role role);
 	public void 		updateRole(Role role);
 	public void 		deleteRole(int roleId);
+	public Role 		findRoleById(int roleId);
 	public Role 		findRoleByName(String roleName);
 	public List<Role> 	listRoles();
 }
---
+++ GestionFastFood/src/main/java/dz/dzDevs/GestionFastFood/DAO/RoleDAOImpl.java
@@ -0,0 +1,42 @@
+package dz.dzDevs.GestionFastFood.DAO;
+
+import java.util.List;
+import org.hibernate.Session;
+import dz.dzDevs.GestionFastFood.Entity.Role;
+import dz.dzDevs.GestionFastFood.Util.HibernateUtil;
+
+public class RoleDAOImpl implements IRoleDAO {
+	private Session session = HibernateUtil.openSession();
+
+	public void createRole(Role role) {
+		session.beginTransaction();
+		session.save(role);
+        session.getTransaction().commit();
+	}
+
+	public void updateRole(Role role) {
+		session.beginTransaction();
+		session.merge(role);
+        session.getTransaction().commit();
+	}
+
+	public void deleteRole(int roleId) {
+		session.beginTransaction();
+		Role role = findRoleById(roleId);
+		session.delete(role);
+        session.getTransaction().commit();	
+	}
+
+	public Role findRoleById(int roleId) {
+		return (Role)session.get(Role.class, roleId);
+	}
+
+	public Role findRoleByName(String roleName) {
+		System.out.println("En cours de realisation...");
+		return null;
+	}
+
+	public List<Role> listRoles() {
+		return session.createQuery("select r from Role r").list();	
+	}
+}
